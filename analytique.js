b1331ef6ac3edd8461f9cf2fd53c667d7afc52b1
Rahimox-alt <53221133+Rahimox-alt@users.noreply.github.com>
2019-07-23 16:43:14 +0100

    Create analytique.js

---
+++ analytique.js
@@ -0,0 +1,11 @@
+<script type='text/javascript'>
+ var _gaq = _gaq || [];
+ _gaq.push([&#39;_setAccount&#39;, &#39;UA-104983096-2&#39;]);
+ _gaq.push([&#39;_trackPageview&#39;]);
+setTimeout(&quot;_gaq.push([&#39;_trackEvent&#39;, &#39;30_seconds&#39;, &#39;read&#39;])&quot;,30000);  // --additional line
+ (function() {
+ var ga = document.createElement(&#39;script&#39;); ga.type = &#39;text/javascript&#39;; ga.async = true;
+ ga.src = (&#39;https:&#39; == document.location.protocol ? &#39;https://ssl&#39; : &#39;http://www&#39;) + &#39;.google-analytics.com/ga.js&#39;;
+ var s = document.getElementsByTagName(&#39;script&#39;)[0]; s.parentNode.insertBefore(ga, s);
+ })();
+</script>
